d81a39d672b61a5efebbc13d483a59d81c9b98c3
emorgan05 <emorgan05@gmail.com>
2018-04-30 15:58:05 -0400

    Changing routing to match the endpoints I want

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'workouts#index'
+  get '/workouts', to: 'workouts#index'
   get '/categories', to: 'categories#index'
+  resources :categories, only: [:show] do
+    resources :workouts, only: [:index], to: 'workouts#category'
+  end
 end
